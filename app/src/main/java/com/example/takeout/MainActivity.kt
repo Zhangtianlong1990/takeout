a6f3dffd551757fc115df990f48a09575c71a5a8
Talon <1126223047@qq.com>
2022-04-05 18:55:21 +0800

    feat : add bottom bar switch logic

--- app/src/main/java/com/example/takeout/MainActivity.kt
+++ app/src/main/java/com/example/takeout/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.takeout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    var main_bottom_bar: LinearLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        main_bottom_bar = findViewById<LinearLayout>(R.id.main_bottom_bar)
+        initBottomBar()
+    }
+
+    private fun initBottomBar() {
+        main_bottom_bar?.run {
+            for (i in 0 until this.childCount){
+                this.getChildAt(i).setOnClickListener {
+                    view -> changeIndex(i)
+                }
+            }
+        }
+
+    }
+
+    private fun changeIndex(index: Int) {
+        main_bottom_bar?.run {
+            for (i in 0 until this.childCount){
+                val child = this.getChildAt(i)
+                if (i == index){
+                    child.isEnabled = false
+                    setEnable(child,false)
+                }else{
+                    setEnable(child,true)
+                }
+            }
+        }
+    }
+
+    private fun setEnable(child: View, isEnable: Boolean) {
+        child.isEnabled = isEnable
+        if (child is ViewGroup){
+            for (i in 0 until child.childCount){
+                child.getChildAt(i).isEnabled = isEnable
+            }
+        }
     }
 }
